3f743883a644863de81d416d0edea45168eb2132
Srijan <srijan.kishu@gmail.com>
2025-05-11 22:43:09 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -27,9 +27,6 @@ app.use('/api/user', userRoute);
 app.use('/api/middleware', middlewareRoute);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 
 export default app;
