99d06de2d151f4a6923d5dfcf629aaa2e56de6d8
Srijan <srijan.kishu@gmail.com>
2025-05-11 22:27:32 +0530

    index.js updated

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,29 @@ import userRoute from './routes/userRoutes.js'
 import middlewareRoute from './routes/middlewareRoute.js'
 
 
-
-
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-connectDB();
+await connectDB();
+
+app.get("/api", (req, res) => {
+  res.status(200).json({messasge:"Hello from backend"});
+});
+
 
 app.use('/api/portfolio', portfolioRoute);
 app.use('/api/user', userRoute);
 app.use('/api/middleware', middlewareRoute);
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/api', (req, res) => {
-   res.status(200).json({message: "Hello from Server"});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
 
 export default app;
